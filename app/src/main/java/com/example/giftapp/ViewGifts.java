9dd4475b5f713615a31aa68052561f1775b8b9de
Chris <chrischitwood@protonmail.ch>
2020-10-18 20:45:01 -0400

    Added logic to view info on gifts in the view_gifts activity.

--- app/src/main/java/com/example/giftapp/ViewGifts.java
+++ app/src/main/java/com/example/giftapp/ViewGifts.java
@@ -2,13 +2,62 @@ package com.example.giftapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewGifts extends Activity {
-    public static final String LOG_TAG = "GiftAppLog: ";
+    static TextView view_gift_result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_gifts);
+        view_gift_result = findViewById(R.id.gift_view);
+
+        view_gift_result.append("\n");
+
+
+        if (Storage.size() == 0)
+            view_gift_result.append("There are no gifts.");
+
+        showGiftInfo();
+
+
     }
+
+    private void showGiftInfo(){
+        for(int i = 0; i < Storage.size(); i++) {
+            displayName(i);
+            displayGift(i);
+            displayPrice(i);
+            displayNotes(i);
+            view_gift_result.append("\n");
+        }
+    }
+
+    private void displayName(int index){
+        view_gift_result.append("For ");
+        view_gift_result.append(Storage.get_x_element(index).getForWhom());
+        view_gift_result.append(":\n");
+    }
+
+    private void displayGift(int index) {
+        view_gift_result.append("Gift: ");
+        view_gift_result.append(Storage.get_x_element(index).getGiftName());
+        view_gift_result.append("\n");
+    }
+
+    private void displayPrice(int index) {
+        view_gift_result.append("Price ");
+        view_gift_result.append(Storage.get_x_element(index).getGiftPrice());
+        view_gift_result.append("\n");
+    }
+
+    private void displayNotes(int index) {
+        view_gift_result.append("Notes: ");
+        view_gift_result.append(Storage.get_x_element(index).getGiftNotes());
+        view_gift_result.append("\n");
+    }
+
+
 }
